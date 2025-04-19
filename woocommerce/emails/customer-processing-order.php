e9b33e6e488e05b40b66f6b4ec7e7f7e707020d2
Ondřej Langr <hi@ondrejlangr.cz>
2025-04-19 17:32:25 +0200

    Removes excessive woo email templates and updates some

--- woocommerce/emails/customer-processing-order.php
+++ woocommerce/emails/customer-processing-order.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Customer processing order email -- this is second in the order
+ * Customer processing order email
  *
  * This template can be overridden by copying it to yourtheme/woocommerce/emails/customer-processing-order.php.
  *
@@ -10,28 +10,43 @@
  * happen. When this occurs the version of the template file will be bumped and
  * the readme will list any important changes.
  *
- * @see https://docs.woocommerce.com/document/template-structure/
- * @package WooCommerce/Templates/Emails
- * @version 3.7.0
+ * @see https://woocommerce.com/document/template-structure/
+ * @package WooCommerce\Templates\Emails
+ * @version 9.8.0
  */
+
+use Automattic\WooCommerce\Utilities\FeaturesUtil;
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
+$email_improvements_enabled = FeaturesUtil::feature_is_enabled( 'email_improvements' );
+
 /*
- * @hooked WC_Emails::email_header() Output the email header 
+ * @hooked WC_Emails::email_header() Output the email header
  */
 do_action( 'woocommerce_email_header', $email_heading, $email ); ?>
 
-<?php /* translators: %s: Customer first name */ ?>
-<p><?php printf( esc_html__( 'Děkujeme za objednávku, %s!', 'woocommerce' ), esc_html( $order->get_billing_first_name() ) ); ?></p>
-<?php 
-	/* translators: %s: Order number */ 
-	// If user selects bank transfer method
-	$isBankTransferPaymentMethod = $order->get_payment_method() === 'bacs';
+<?php echo $email_improvements_enabled ? '<div class="email-introduction">' : ''; ?>
+<p>
+<?php
+if ( ! empty( $order->get_billing_first_name() ) ) {
+	/* translators: %s: Customer first name */
+	printf( esc_html__( 'Hi %s,', 'woocommerce' ), esc_html( $order->get_billing_first_name() ) );
+} else {
+	printf( esc_html__( 'Hi,', 'woocommerce' ) );
+}
 ?>
-<p><?php printf( esc_html__( 'Obdrželi jsme Vaši objednávku #%s a nyní na ní pracujeme.' . ($isBankTransferPaymentMethod ? "<br/><b style='color:red'>Zvolili jste platbu bankovním převodem! V případě, že platbu neobdržíme do sedmi dnů, objednávku rušíme a zboží vracíme zpět do nabídky. Děkujeme za pochopení.</b>" : ""), 'woocommerce' ), esc_html( $order->get_order_number() ) ); ?></p>
+</p>
+<?php if ( $email_improvements_enabled ) : ?>
+	<p><?php esc_html_e( 'We’ve received your order and will let you know when it’s on its way to you!', 'woocommerce' ); ?></p>
+	<p><?php esc_html_e( 'Here’s a reminder of what you’ve ordered:', 'woocommerce' ); ?></p>
+<?php else : ?>
+	<?php /* translators: %s: Order number */ ?>
+	<p><?php printf( esc_html__( 'Just to let you know &mdash; we\'ve received your order #%s, and it is now being processed:', 'woocommerce' ), esc_html( $order->get_order_number() ) ); ?></p>
+<?php endif; ?>
+<?php echo $email_improvements_enabled ? '</div>' : ''; ?>
 
 <?php
 
@@ -58,12 +73,12 @@ do_action( 'woocommerce_email_customer_details', $order, $sent_to_admin, $plain_
  * Show user-defined additional content - this is set in each email's settings.
  */
 if ( $additional_content ) {
+	echo $email_improvements_enabled ? '<table border="0" cellpadding="0" cellspacing="0" width="100%"><tr><td class="email-additional-content">' : '';
 	echo wp_kses_post( wpautop( wptexturize( $additional_content ) ) );
+	echo $email_improvements_enabled ? '</td></tr></table>' : '';
 }
 
 /*
  * @hooked WC_Emails::email_footer() Output the email footer
  */
 do_action( 'woocommerce_email_footer', $email );
-
-
